b52420f4c91ce97e3a92c021997a35f8501b6ae0
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-14 16:05:11 -0300

    Initializing Exercise 02 - Interface

---
+++ Exercise2Interface/Entities/Installments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2Interface.Entities
+{
+    class Installments
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installments(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+
+    }
+}
---
+++ Exercise2Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise2Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
